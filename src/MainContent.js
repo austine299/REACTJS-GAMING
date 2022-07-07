80c3646822719f15b4ccedaf2debfe3a8b6b629b
ugochukwu <ugochukwuaugustine299@gmail.com>
2022-07-06 23:54:58 -0700

    gaming first commit

---
+++ src/MainContent.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+export default function MainContent(){
+    return(
+      <div className="maincontent">
+        <h1 className="main-title">Fun facts about react</h1>
+        <ul className="main-facts">
+          <li>Was first released in 2013</li>
+          <li>Was originallytcreated by Jordan Walke</li>
+          <li>Has well over 100k stars on Github </li>
+          <li>Is maintained by Facebook</li>
+          <li>Powers thousands of enterprise apps, including mobel apps</li>
+        </ul>
+      </div>
+    )
+}
+  
